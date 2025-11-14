c32ee2df0f86c44036e63faaa614cdabb5d71ad8
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2025-11-14 13:31:12 +0530

    updated hero section and rmoved 3d component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Planet } from "../components/Planet";
+// import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
